172911727b8b139195d6efa51a1363dea77d281e
Fabricio <u19218420@utp.edu.pe>
2024-09-02 03:26:27 -0500

    flow install and update

---
+++ update.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+set "api_url=https://api.github.com/repos/FabricioMR22/match-day/releases/latest"
+
+curl -s %api_url% > temp.json
+
+for /f "tokens=*" %%a in ('findstr /r /c:"\"browser_download_url\"" temp.json') do set "download_url=%%a"
+set "download_url=%download_url:~24,-1%"
+
+curl -LO %download_url%
+
+powershell -command "Expand-Archive -Path 'match.zip' -DestinationPath '.'"
+
+del temp.json
+del match.zip
+
+endlocal
